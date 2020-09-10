0ea620782cd9496c0d2a6b68aa53a8b3c26a3722
MorbidMiyako <57117884+MorbidMiyako@users.noreply.github.com>
2020-09-10 16:23:58 +0100

    simple switching api, next up processing incoming body

--- index.php
+++ index.php
@@ -36,3 +36,32 @@ implement switch statement to react accordingly to the request method
 create response for GET and POST request method
 
 */
+
+//lets store the request method
+$request_method = $_SERVER['REQUEST_METHOD'];
+echo "request method";
+echo "\n";
+echo $request_method;
+echo "\n";
+
+response($request_method);
+
+function response($request_method) {
+    switch($request_method) {
+        case 'GET': 
+            echo "GET";
+            echo "\n";
+            // break statements are essential for switches, good reminder haha
+            // this seems to favour the choice of chaining cases witht he same output over saving the hassle of writing break statements.
+            break;
+        case 'POST':
+            echo "POST";
+            echo "\n";
+            break;
+        default:
+            echo "request_method unsupported";
+            echo "\n";
+            break;
+
+    }
+}
