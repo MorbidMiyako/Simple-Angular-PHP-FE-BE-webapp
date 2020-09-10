c5f29b31b2b1b2bf551f0b9cfdb42c2000563133
MorbidMiyako <57117884+MorbidMiyako@users.noreply.github.com>
2020-09-10 14:04:54 +0100

    basic hello world api in php

---
+++ learning PHP/basic_php_stuff.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+###############################
+Seeing how messy the objects can be in PHP
+###############################
+
+$array = array(
+	"foo" => "bar",
+	2 => "hey",
+	"5" => "nah",
+	"okay?",
+	"6.5" => "bs",
+	6.5 => "actual bs"
+);
+
+Print var_dump($array);
+
+*/
+
+/*
+###############################
+implode and explode are join and split functions
+###############################
+
+Print implode("\n ",$_SERVER);
+Print "\n";
+Print '$_SERVER';
+Print "$_SERVER";
+*/
+
+/*
+###############################
+###############################
+
+response(200,"Hello, World!");
+function response($status,$status_message)
+{
+	echo $status_message
+}
+
+*/
